9035b7f61cbe1fba24c650cc52ff801be1b3b4e4
Cheruiyot Evans <152331758+CheruiyotDev@users.noreply.github.com.>
2024-09-19 14:21:18 +0300

    Add mondayWork, staurdayFun and wrapAdjective Functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function mondayWork(string = 'go to the office') {
+  return `This Monday, I will ${string}.`
+}
+
+function saturdayFun(work = 'roller-skate') {
+  return `This Saturday, I want to ${work}!`
+}
+
+function wrapAdjective(outer = '*') {
+  if (outer == '*') {
+    return function inner(user = "special") {
+      return `You are ${outer}a hard worker${outer}!`
+    } 
+  } else if (outer == '||') {
+    return function inner(user = "special") {
+      return `You are ${outer}a dedicated programmer${outer}!`
+    }
+  }
+}
+// function wrapAdjective(name = '*') {
+//   if (name == '*') {
+//     return function inner(user = "special") {
+//       return `You are ${name}a hard worker${name}!`
+//     } 
+//   } else if (name == '||') {
+//     return function inner(user = "special") {
+//       return `You are ${name}a dedicated programmer${name}!`
+//     }
+//   }
+// }
